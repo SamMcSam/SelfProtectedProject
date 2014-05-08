aa9152a6cf660052954398d017f3ead3f5fa0216
SamMcSam <ilakebonza@hotmail.com>
2014-05-09 01:12:35 +0200

    finished prototype

--- Code/Prototype/filewriter/gen/com/example/filewriter/R.java
+++ Code/Prototype/filewriter/gen/com/example/filewriter/R.java
@@ -1077,9 +1077,11 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int ic_action_search=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
+        public static final int action_ajout=0x7f05003f;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,7 +1093,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1111,6 +1113,7 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int list=0x7f05003d;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1137,6 +1140,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int text1=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1195,7 +1199,8 @@ containing a value of this type.
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
         public static final int fragment_main=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int list_row=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1241,7 +1246,8 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_ajout=0x7f0a000f;
+        public static final int action_settings=0x7f0a0010;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
     }
--- Code/Prototype/filewriter/src/com/example/filewriter/MainActivity.java
+++ Code/Prototype/filewriter/src/com/example/filewriter/MainActivity.java
@@ -1,27 +1,42 @@
 package com.example.filewriter;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
+	
+	//pour affichage list
+	private ListView lv;
+	ArrayList<String> item;
+	private ArrayAdapter<String> adapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		 lv = (ListView)findViewById(R.id.list);
+		
 		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+			//recharge
 		}
+		
+		//POUR RETOUR EN ARRIERE
+		//getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		//+ ajout parent dans manifest
+		
 	}
 
 	@Override
@@ -38,27 +53,114 @@ public class MainActivity extends ActionBarActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
+			if (id == R.id.action_ajout) {
+				createTheFile("test");
+			}
+			if (id == R.id.action_settings) {
+				//createTheFile("test");
+			}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
+	
+	@Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //saveState();
+        //outState.putSerializable(NotesDbAdapter.KEY_ROWID, mRowId);
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        
+        item = new ArrayList<String>();
+        
+        //list the files
+        File file[] = this.getAlbumStorageDir("MySmartContent").listFiles();
+        for (int i=0; i < file.length; i++)
+        {
+        	item.add(file[i].getName());
+        }
+        
+        String[] from =  item.toArray(new String[item.size()]);
+        
+        this.adapter = new ArrayAdapter<String>(this,R.layout.list_row, R.id.text1, from);
+        lv.setAdapter(this.adapter);  
+        
+    }
+	
+	
+	// FOR WRITING
+	//----------------------------------------------------------
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
+	public void createTheFile(String filename){
 
-		public PlaceholderFragment() {
+		String string = "Hello world!";
+		
+		/*
+		FileOutputStream outputStream;
+
+		try {
+		  outputStream = openFileOutput(filename, Context.MODE_WORLD_READABLE);
+		  outputStream.write(string.getBytes());
+		  outputStream.close();
+		} catch (Exception e) {
+		  e.printStackTrace();
 		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
+		*/
+		try{
+			FileWriter writer = new FileWriter(this.getAlbumStorageDir("MySmartContent") + "/" + filename + ".smart", false);
+			writer.write(string);
+			writer.flush();
+			writer.close();
 		}
+		catch(Exception e){
+			Toast.makeText(this, "Error while writing", Toast.LENGTH_SHORT)
+	          .show();
+		}
+		
+	}
+	
+	/* Checks if external storage is available for read and write */
+	public boolean isExternalStorageWritable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state)) {
+	        return true;
+	    }
+	    return false;
 	}
 
+	/* Checks if external storage is available to at least read */
+	public boolean isExternalStorageReadable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state) ||
+	        Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
+	
+	@SuppressLint("NewApi")
+	public File getAlbumStorageDir(String albumName) {
+	    // Get the directory for the user's public pictures directory. 
+	    //File file = new File(this.getExternalFilesDir(null), albumName);
+	    File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), albumName);
+	    
+	    //creates directory
+	    if (!file.mkdirs()) {
+	    	if (!file.isDirectory()){
+	    		Toast.makeText(this, "Directory not created", Toast.LENGTH_SHORT)
+		          .show();
+	    	}
+	    }
+	    return file;
+	}
+	
 }
